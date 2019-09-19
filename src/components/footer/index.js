61d551176262e7040b0abfae4749213848192634
mantismb <matt.d.baldwin@gmail.com>
2019-09-18 19:03:22 -0700

    sweet corner v1 feature set 2 | Matthew Baldwin

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './footer.scss';
 
 export default props => {
+    const year = new Date().getFullYear();
     return (
         <footer>
         <div className="dots"></div> 
-        <div className="date">&copy; 2019 all rights reserved</div>
-        <div><i className="fa fa-phone"></i>800-264-2099</div> 
+        <div className="date">&copy; {year} all rights reserved</div>
+        <div className="phone"><i className="fa fa-phone"></i>800-264-2099</div> 
         </footer>
     );
 }
