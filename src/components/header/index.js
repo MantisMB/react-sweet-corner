0702ed35422eb8c24f14dcb615688d47c773dd00
mantismb <matt.d.baldwin@gmail.com>
2019-09-23 19:04:30 -0700

    Sweet corner V1 feature set 4 | Matthew Baldwin

---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './header.scss';
+import Nav from '../nav';
+
+export default props => {
+    return (
+        <header>
+        <div className="top-image">
+        </div>
+        <Nav />
+        <div className="main-icon"></div>
+        <h2>We deliver cupcakes for the important events in your life!</h2>
+        </header>
+    );
+}
